8bc306e6e288ac93f5b1e0c4ffc9d1ddf9f8ae7a
Mustafa Ali <alimustafa0987@gmail.com>
2020-11-07 18:30:37 -0500

    added dna

--- script.js
+++ script.js
@@ -106,11 +106,46 @@ function Vector(x,y) {
     }
 }
 
-function DNA() {
-    //
+function DNA(genes) {
+    
+    if (genes) {
+        this.genes = genes;
+    }
+    
+    else {
+        this.genes = [];
+        for (var i = 0; i < lifespan; i++) {
+            this.genes[i] = new Vector();
+            this.genes[i].setMag(maxforce);
+        }
+    }
+    
+    this.crossover = function(partner) {
+        var newgenes = [];
+        var mid = floor(random(this.genes.length));
+        for (var i = 0; i < this.genes.length; i++) {
+            if (i > mid) {
+                newgenes[i] = this.genes[i];
+            }
+            else {
+                newgenes[i] = partner.genes[i];
+            }
+        }
+        return new DNA(newgenes);
+    }
+
+    // Adds random mutation to the genes to add variance.
+    this.mutation = function() {
+        for (var i = 0; i < this.genes.length; i++) {
+            if (random(1) < 0.01) {
+                this.genes[i] = new Vector();
+                this.genes[i].setMag(maxforce);
+            }
+        } 
+    }
 }
 
-Population = () => {
+function Population{
     //
 }
 
